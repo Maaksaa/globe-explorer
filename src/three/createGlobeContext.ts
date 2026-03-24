f6f5c1188827619242f516774021c3c6109bda44
Maksim Demidov <maaksaa@gmail.com>
2026-03-24 16:15:21 +0100

    feat: add module in GlobeContext

--- src/three/createGlobeContext.ts
+++ src/three/createGlobeContext.ts
@@ -1,10 +1,16 @@
 import * as THREE from "three/webgpu";
 import { CoreContext } from "@vladkrutenyuk/three-kvy-core";
+import { CameraControlsModule } from "./modules/CameraControlsModule";
+import { TweenModule } from "./modules/TweenModule";
+
+export type GlobeModules = {
+	cameraControls: CameraControlsModule;
+	tween: TweenModule;
+};
 
 export async function createGlobeContext() {
 	const renderer = new THREE.WebGPURenderer({ antialias: true });
 	renderer.setPixelRatio(window.devicePixelRatio);
-
 	await renderer.init();
 
 	const camera = new THREE.PerspectiveCamera(45, 1, 0.1, 1000);
@@ -13,10 +19,14 @@ export async function createGlobeContext() {
 	const scene = new THREE.Scene();
 	const clock = new THREE.Clock();
 
-	return CoreContext.create({
+	return CoreContext.create<GlobeModules>({
 		renderer,
 		camera,
 		scene,
 		clock,
+		modules: {
+			cameraControls: new CameraControlsModule(),
+			tween: new TweenModule(),
+		},
 	});
 }
